26c0de69f2349f666afeecc957f9ac07c51c408f
yenjamin <94yenho@gmail.com>
2022-03-14 12:45:24 +0200

    add more words and speed difference

--- word.py
+++ word.py
@@ -11,6 +11,15 @@ class word():
         #self.displayword = RandomWords().get_random_word()
         self.displayword = random.choice(filehandler.readfile())
         self.x = random.randint(300,700)
+        i = len(self.displayword)
+        if len(self.displayword) <= 4:
+            while 4 - i != 0:
+                self.wordSpeed += 0.1
+                i += 1
+        else:
+            while i - 4 != 0:
+                self.wordSpeed -= 0.1
+                i -= 1
     yourWord = ""
     x = random.randint(300,700)
     y = 200
